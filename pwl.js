13912051336928e9aff944159719fd2b5fa1eea1
Paul Anderson <panderson@add123.com>
2014-08-29 11:28:21 -0400

    updating for new api

--- pwl.js
+++ pwl.js
@@ -5,69 +5,79 @@ pwl.engaged = false;
 pwl.userRestoredMessageDefault = '@username rejoined within 1 hour of leaving and has been restored to position @position in the wait list.';
 pwl.userRestoredMessage = pwl.userRestoredMessageDefault;
 
-pwl.userLeaveCallback = function(user) {
-  pwl.data[user.id] = {};
-  pwl.data[user.id].leftAt = new Date().getTime();
-  pwl.data[user.id].wlIndex = user.wlIndex;
-}
-
-pwl.userJoinCallback = function(user) {
-  if(pwl.data[user.id]) {
-    if(pwl.shouldUserBeRestored(pwl.data[user.id])) {
-      var restoreToPosition = Math.min(API.getWaitList().length, pwl.data[user.id].wlIndex + 1);
-      if(pwl.fullAuto && API.hasPermission(null,API.ROLE.MANAGER)) {
-        var moveDJ = function(usersInWaitList) {
-          var userFound = false;
-          for(var i=0;i<usersInWaitList.length;i++) {
-            if(usersInWaitList[i].id = user.id) {
-              API.moderateMoveDJ(user.id, restoreToPosition);
-              API.off(API.WAIT_LIST_UPDATE, moveDJ);
-              break;
-            }
-          }
-        };
-        API.on(API.WAIT_LIST_UPDATE, moveDJ);
-        API.moderateAddDJ(user.id);
-        API.sendChat('PWL: ' + pwl.userRestoredMessage.replace(/@username/i,user.username).replace(/@position/i,restoreToPosition));
-      } else {
-        API.sendChat('PWL: ' + user.username + ' rejoined within 1 hour of leaving and should be restored to position ' + restoreToPosition + ' in the wait list.');
+pwl.waitListUpdateCallback = function(waitList) {
+  var timestamp = new Date().getTime();
+  for(i=0;i<waitList.length;i++) {
+    if(!pwl.data[waitList[i].id]) {
+      pwl.data[waitList[i].id] = {};
+    }
+    pwl.data[waitList[i].id].wlIndex = i;
+    pwl.data[waitList[i].id].timestamp = timestamp;
+  }
+  for(var k in pwl.data) {
+    if(pwl.data.hasOwnProperty(k)) {
+      if(pwl.data[k].timestamp !== timestamp && $.isEmptyObject(API.getUser(k))) {
+        pwl.data[k].leftAt = timestamp;
       }
     }
   }
-}
+};
+
+pwl.userJoinCallback = function(user) {
+  if(pwl.data[user.id] && pwl.shouldUserBeRestored(pwl.data[user.id])) {
+    var restoreToPosition = Math.min(API.getWaitList().length, pwl.data[user.id].wlIndex + 1);
+    if(pwl.fullAuto && API.hasPermission(null,API.ROLE.MANAGER)) {
+      var moveDJ = function(usersInWaitList) {
+        var userFound = false;
+        for(i=0;i<usersInWaitList.length;i++) {
+          if(usersInWaitList[i].id === user.id) {
+            API.moderateMoveDJ(user.id, restoreToPosition);
+            API.off(API.WAIT_LIST_UPDATE, moveDJ);
+            break;
+          }
+        }
+      };
+      API.on(API.WAIT_LIST_UPDATE, moveDJ);
+      API.moderateAddDJ(user.id);
+      API.sendChat('PWL: ' + pwl.userRestoredMessage.replace(/@username/i,user.username).replace(/@position/i,restoreToPosition));
+    } else {
+      API.sendChat('PWL: ' + user.username + ' rejoined within 1 hour of leaving and should be restored to position ' + restoreToPosition + ' in the wait list.');
+    }
+  }
+};
 
 pwl.shouldUserBeRestored = function(storedUserData) {
   var now = new Date().getTime();
-  return storedUserData.leftAt + 5000 < now && storedUserData.leftAt + 3600000 > now && storedUserData.wlIndex > -1;
-}
+  return storedUserData.leftAt + 5000 < now && storedUserData.leftAt + 3600000 > now && storedUserData.wlPosition > -1;
+};
 
 pwl.chatCallback = function(chatData) {
   if(chatData.message[0] !== ';' && chatData.message[0] !== '!') return;
   var command = chatData.message.toLowerCase().substring(1);
   if(command === 'pwlrunning') {
     if(pwl.engaged) {
-      API.sendChat('PWL is running, @' + chatData.from);
+      API.sendChat('PWL is running, @' + chatData.un);
     }
   } else if(command === 'wtfispwl') {
     if(pwl.engaged) {
       API.sendChat('PWL is a Persistent Wait List. If you leave while in the wait list but return within an hour, it\'ll put you back where you were.');
     }
   }
-}
+};
 
 pwl.init = function(fullAuto) {
   pwl.stop(true);
   pwl.fullAuto = fullAuto;
+  API.on(API.USER_JOIN, pwl.userJoinCallback);
+  API.on(API.USER_LEAVE, pwl.userLeaveCallback);
+  API.on(API.CHAT, pwl.chatCallback);
+  pwl.engaged = true;
   if(fullAuto) {
     API.sendChat('PWL: Engaged in full auto.');
   } else {
     API.sendChat('PWL: Engaged.');
   }
-  API.on(API.USER_JOIN, pwl.userJoinCallback);
-  API.on(API.USER_LEAVE, pwl.userLeaveCallback);
-  API.on(API.CHAT, pwl.chatCallback);
-  pwl.engaged = true;
-}
+};
 
 pwl.stop = function(stealth) {
   if(!stealth) {
@@ -77,11 +87,11 @@ pwl.stop = function(stealth) {
   API.off(API.USER_LEAVE, pwl.userLeaveCallback);
   API.off(API.CHAT, pwl.chatCallback);
   pwl.engaged = false;
-}
+};
 
 pwl.gui = $('<div/>');
 pwl.gui.attr('id','pwlGui');
-pwl.gui.attr('style',"z-index:999;padding:10px;text-align:center;background:#282C35;border-radius:5px;position:absolute;left:10px;top:50px");
+pwl.gui.attr('style',"z-index:999;padding:10px;text-align:center;background:#282C35;border-radius:5px;position:absolute;left:10px;top:75px");
 pwl.gui.toggle = $('<div/>');
 pwl.gui.toggle.attr('id','pwlGui');
 pwl.gui.toggle.css('cursor','pointer');
@@ -105,8 +115,8 @@ pwl.gui.customize.input.val(pwl.userRestoredMessage);
 pwl.gui.normalColor = '#eee';
 pwl.gui.hoverColor = '#00b5e6';
 
-pwl.gui.toggle.hover(function(){$(this).css('color',pwl.gui.hoverColor);}, function(){$(this).css('color',pwl.gui.normalColor);})
-pwl.gui.customize.hover(function(){$(this).css('color',pwl.gui.hoverColor);}, function(){$(this).css('color',pwl.gui.normalColor);})
+pwl.gui.toggle.hover(function(){$(this).css('color',pwl.gui.hoverColor);}, function(){$(this).css('color',pwl.gui.normalColor);});
+pwl.gui.customize.hover(function(){$(this).css('color',pwl.gui.hoverColor);}, function(){$(this).css('color',pwl.gui.normalColor);});
 
 pwl.gui.append(pwl.gui.toggle);
 pwl.gui.append('<br>');
